daf301bab3f4623c4661b43f21597e9bc63f8d01
ClementinaO <clementinaorogun@gmail.com>
2022-08-22 15:34:03 +0000

    for visa project

---
+++ Visa1.sh
@@ -0,0 +1,8 @@
+ieekkmmjjncjncmj cmk nkn ckmn ckn vk
+cbj jcM Skmckan lvaklnv akl;nvbk
+knvb jln bvjmblkjvb njl njmm, njkmc
+mxnfjhfjknlkknj uhjvnmklvhun a
+sjnkbh sfujnvdujrglluyhn mfvlmvdzkm,v x
+xm nbjhndzs  nkljmm sd l.knc zjum dzc/,.nkn m,v ,lhgbjm, amnhuhgakejeskf nvf vnn hjb n hn
+nbjn nmjm, njm n,jk mvszd  nbjhc mnxv nm, jm jmzdv nk vfnbknm ,nklm zcsj vxfknvfxsjkfvx bsn xjvmzxdvnx jmnx m, nxkjxfbs,m xfvmnh n bfx,jhmnrgsdtkkk;poii9okm, dthf km,jhihjmdzsacfjhnjk,htdbn,,lkm ,vlmmkmk ijkmvfmkokmkgbsjknbm ,xfkn
+
